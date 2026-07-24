6d57881154144b08c674af3b43a89976c43ef0b0
Arian Mahmutovic <arian.business.contact@gmail.com>
2026-07-24 15:25:18 -0400

    first commit

---
+++ submit.js
@@ -0,0 +1,69 @@
+const form = document.querySelector("#join-form");
+const joinContent = document.querySelector("#join-content");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    try {
+
+        const response = await fetch(form.action, {
+            method: "POST",
+            body: data,
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+
+
+        if (response.ok) {
+
+            joinContent.innerHTML = `
+                <h2>You're all set.</h2>
+
+                <h3>
+                    We appreciate your interest in Project Epictetus.
+                </h3>
+
+                <h3>
+                    Would you like to help us build a better platform by
+                    answering a short survey?
+                </h3>
+
+                <br>
+                <br>
+                <br>
+
+                <a href="https://forms.gle/FokuuMfcSi85DmbKA" 
+                   target="_blank"
+                   class="button">
+                    Take Survey
+                </a>
+            `;
+
+        } else {
+
+            joinContent.innerHTML = `
+                <h2>Something went wrong.</h2>
+
+                <h3>
+                    Please try again later.
+                </h3>
+            `;
+
+        }
+
+    } catch (error) {
+
+        joinContent.innerHTML = `
+            <h2>Connection error.</h2>
+
+            <h3>
+                Please check your internet connection and try again.
+            </h3>
+        `;
+
+    }
+
+});
